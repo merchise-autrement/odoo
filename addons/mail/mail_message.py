4bd2c25c36b66acf7ea3f7a9796ef4b36181c7b8
Manuel Vázquez Acosta <mva.led@gmail.com>
2015-08-29 17:52:08 -0400

    [mail] Avoid ephemeral list by passing the generator.

--- addons/mail/mail_message.py
+++ addons/mail/mail_message.py
@@ -41,7 +41,7 @@ def decode(text):
         text = decode_header(text.replace('\r', ''))
         # The joining space will not be needed as of Python 3.3
         # See https://hg.python.org/cpython/rev/8c03fe231877
-        return ' '.join([tools.ustr(x[0], x[1]) for x in text])
+        return ' '.join(tools.ustr(x[0], x[1]) for x in text)
 
 class MLStripper(HTMLParser):
     def __init__(self):
