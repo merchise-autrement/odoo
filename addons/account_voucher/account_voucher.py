5fa9ba6c2e25c1ce6a7a8cd4795fcebac5344c1b
Andro Font <andro@merchise.org>
2016-02-01 13:24:43 -0500

    [ADD] Register move line name always no matter how the payment was done

--- addons/account_voucher/account_voucher.py
+++ addons/account_voucher/account_voucher.py
@@ -1028,6 +1028,7 @@ class account_voucher(osv.osv):
         :rtype: dict
         '''
         voucher = self.pool.get('account.voucher').browse(cr,uid,voucher_id,context)
+        move = self.pool.get('account.move.line').browse(cr,uid,move_id,context)
         debit = credit = 0.0
         # TODO: is there any other alternative then the voucher type ??
         # ANSWER: We can have payment and receipt "In Advance".
@@ -1042,7 +1043,7 @@ class account_voucher(osv.osv):
         sign = debit - credit < 0 and -1 or 1
         #set the first line of the voucher
         move_line = {
-                'name': voucher.name or '/',
+                'name': move.name or '/',
                 'debit': debit,
                 'credit': credit,
                 'account_id': voucher.account_id.id,
