1a9bab9b5f032f3fe6d43dff3ebf084558b166e1
Manuel Vázquez Acosta <mva.led@gmail.com>
2017-02-24 19:41:11 -0500

    sentry, celery: Integrate sentry and celery.

--- openerp/cli/celery.py
+++ openerp/cli/celery.py
@@ -22,6 +22,22 @@ from . import Command
 
 class Celery(Command):
     def run(self, cmdargs):
+        import logging
         from celery.bin.celery import main as _main
         from openerp.jobs import app  # noqa: discover the app
+        from openerp.sentrylog import get_client
+        from raven.contrib.celery import register_signal, register_logger_signal
+        client = get_client()
+        # register a custom filter to filter out duplicate logs
+        register_logger_signal(client)
+        # The register_logger_signal function can also take an optional
+        # argument `loglevel` which is the level used for the handler created.
+        # Defaults to `logging.ERROR`
+        register_logger_signal(client, loglevel=logging.INFO)
+        # hook into the Celery error handler
+        register_signal(client)
+        # The register_signal function can also take an optional argument
+        # `ignore_expected` which causes exception classes specified in
+        # Task.throws to be ignored
+        register_signal(client, ignore_expected=True)
         _main(argv=['celery', ] + cmdargs)
