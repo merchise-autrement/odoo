dc355c4f3aa2b08870914fc8c7179b3c2963933d
Manuel Vázquez Acosta <mva.led@gmail.com>
2017-02-27 14:49:44 -0500

    celery: Don't assume Sentry is going to be configured.

--- openerp/cli/celery.py
+++ openerp/cli/celery.py
@@ -28,16 +28,17 @@ class Celery(Command):
         from openerp.sentrylog import get_client
         from raven.contrib.celery import register_signal, register_logger_signal
         client = get_client()
-        # register a custom filter to filter out duplicate logs
-        register_logger_signal(client)
-        # The register_logger_signal function can also take an optional
-        # argument `loglevel` which is the level used for the handler created.
-        # Defaults to `logging.ERROR`
-        register_logger_signal(client, loglevel=logging.INFO)
-        # hook into the Celery error handler
-        register_signal(client)
-        # The register_signal function can also take an optional argument
-        # `ignore_expected` which causes exception classes specified in
-        # Task.throws to be ignored
-        register_signal(client, ignore_expected=True)
+        if client:
+            # register a custom filter to filter out duplicate logs
+            register_logger_signal(client)
+            # The register_logger_signal function can also take an optional
+            # argument `loglevel` which is the level used for the handler created.
+            # Defaults to `logging.ERROR`
+            register_logger_signal(client, loglevel=logging.INFO)
+            # hook into the Celery error handler
+            register_signal(client)
+            # The register_signal function can also take an optional argument
+            # `ignore_expected` which causes exception classes specified in
+            # Task.throws to be ignored
+            register_signal(client, ignore_expected=True)
         _main(argv=['celery', ] + cmdargs)
