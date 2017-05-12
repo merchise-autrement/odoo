486c93184a9230e0d2c04b9678c137f903731025
Johanny <johanny@merchise.org>
2017-05-11 20:23:20 -0400

    Add project tasks gantt view.

---
+++ odoo/addons/project_gantt/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# odoo.addons.project_gantt.__init__
+# ---------------------------------------------------------------------
+# Copyright (c) 2017 Merchise Autrement [~º/~]
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+# Created on 2017-05-02
