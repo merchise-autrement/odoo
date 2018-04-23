9e87176e9df048969ec5b24d1699e1b277f0b2cd
Manuel Vázquez Acosta <manuel@merchise.org>
2018-04-23 17:25:25 -0400

    Merge commit '2d9f3b62007ffbddec2e4ecf1bcea231efb401a3' as 'sources/dists/odoo'

---
+++ addons/sales_team/models/res_users.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    sale_teams = fields.Many2many(
+        'crm.team',
+        string="Sales Teams",
+        relation='sale_member_rel',
+        column1='user_id',
+        column2='team_id',
+        help="Sales Teams the user is member of."
+    )
+
+    @api.model
+    def create(self, vals):
+        # Assign the new user in the sales team if there's only one sales team of type `Sales`
+        user = super(ResUsers, self).create(vals)
+        if user.has_group('sales_team.group_sale_salesman') and not user.sale_team_id:
+            teams = self.env['crm.team'].search([('team_type', '=', 'sales')])
+            if len(teams.ids) == 1:
+                user.sale_teams |= teams
+        return user
---
+++ addons/web_celery/__manifest__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Copyright (c) Merchise Autrement [~º/~] and Contributors
+# All rights reserved.
+#
+# This is free software; you can do what the LICENCE file allows you to.
+#
+
+dict(
+    name='Celery Integration',
+    author='Merchise Autrement [~º/~]',
+    category='Hidden',
+    description='Web API for Celery Integration',
+    summary='Web API for Celery Integration',
+    depends=['web', 'bus'],
+    data=['views/assets.xml', ],
+    qweb=['static/src/xml/*.xml', ],
+    auto_install=True,
+    installable=True,
+    application=False,
+)
