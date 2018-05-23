678b54f82843716c4122695ac361bb01e477c825
Manuel Vázquez Acosta <manuel@merchise.org>
2018-05-23 18:51:44 -0400

    Revert "signals: Remove signals module."
    This reverts commit d1ed11828d2b338003b2cfbe41d20eb7baa1d27d.
    
    The signals need to be bootstraped before any model is loaded.  Otherwise
    models that overwrite any of the signaled methods may be by-passed.

--- odoo/__init__.py
+++ odoo/__init__.py
@@ -73,6 +73,7 @@ import workflow
 from . import models
 from . import fields
 from . import api
+from . import signals  # noqa
 from odoo.tools.translate import _
 
 #----------------------------------------------------------
