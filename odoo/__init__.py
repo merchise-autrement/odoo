93437afb76bdf13fbff3952acfc07677a67699ef
Manuel Vázquez Acosta <manuel@merchise.org>
2018-05-24 13:05:03 -0400

    signals: Remove signals module.
    This reverts commit 678b54f82843716c4122695ac361bb01e477c825.

--- odoo/__init__.py
+++ odoo/__init__.py
@@ -73,7 +73,6 @@ import workflow
 from . import models
 from . import fields
 from . import api
-from . import signals  # noqa
 from odoo.tools.translate import _
 
 #----------------------------------------------------------
