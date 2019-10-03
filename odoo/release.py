e3e03484eac2bdd02b7a220387846f5ad99cdce3
Manuel Vázquez Acosta <manuel@merchise.org>
2019-10-03 09:32:12 -0400

    Start release 191.
    The problem: Putting our release in the addon's versions creates the problem
    with our rolling-releases schema that we can't bump the version till late
    in the development cycle.
    
    But then migrations that reach master are not run in the 'development' and
    'staging' branches, which causes some errors in those environments.
    
    The solution:
    
    Use a single-number for our releases and put it as the second number of the
    Odoo version.  Our next release was named 1.9.1, so we mark the next release
    as 12.191.  Our numbers will increased by one: 192, 193, ...
    
    Addons can have a numbering of their own and bump the version whenever they
    want.

--- odoo/release.py
+++ odoo/release.py
@@ -10,9 +10,7 @@ RELEASE_LEVELS_DISPLAY = {ALPHA: ALPHA, BETA: BETA, RELEASE_CANDIDATE: "rc", FIN
 #  (6,1,0,'beta',0) < (6,1,0,'candidate',1) < (6,1,0,'candidate',2)
 #  (6,1,0,'candidate',2) < (6,1,0,'final',0) < (6,1,2,'final',0)
 
-STAMP = ".1.9.0"  # merchise: our current release
-
-version_info = (12, 0, 0, FINAL, STAMP, "")
+version_info = (12, 191, 0, FINAL, "merchise", "")
 version = (
     ".".join(str(s) for s in version_info[:2])
     + RELEASE_LEVELS_DISPLAY[version_info[3]]
