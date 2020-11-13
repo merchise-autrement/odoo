f2c161ea03180d1817114771e4d42864b1dcaa6e
Manuel Vázquez Acosta <manuel@merchise.org>
2020-11-13 09:27:45 -0500

    celery cli: Improve support of flower options.
    Flower does not align prettily with Celery's click.  They convert 'options' to
    'settings' and those doesn't align 1:1.
    
    So we're copying/adapting a bunch of code from Flower until it works
    again (maybe after Celery 5.0.3
    https://github.com/celery/celery/pull/5718#issuecomment-725996946)

--- odoo/cli/celery.py
+++ odoo/cli/celery.py
@@ -59,7 +59,13 @@ class Celery(Command):
 
 
 # TODO: Add all possible Flower options.
+# Most of this is copied from flower's command code.
 @celery.command(cls=CeleryCommand)
+@click.option(
+    "--debug",
+    default=False,
+    is_flag=True,
+)
 @click.option("--port", default=5555)
 @click.option(
     "--logging",
@@ -68,8 +74,10 @@ class Celery(Command):
 )
 @click.option(
     "--basic_auth",
+    "--basic-auth",
     type=str,
     multiple=True,
+    callback=lambda ctx, param, value: list(value),
 )
 @click.option(
     "--log-to-stderr",
@@ -85,9 +93,13 @@ def flower(ctx, **kwargs):
     from flower.app import Flower
     from flower.urls import settings
 
-    kwargs["basic_auth"] = list(kwargs["basic_auth"])
+    apply_env_options(options)
     for option, value in kwargs.items():
         setattr(options, option, value)
+    extract_settings(options, settings)
+    setup_logging(options)
+
+    ctx.obj.app.loader.import_default_modules()
     flower = Flower(capp=ctx.obj.app, options=options, **settings)
     atexit.register(flower.stop)
 
@@ -102,5 +114,75 @@ def flower(ctx, **kwargs):
     except (KeyboardInterrupt, SystemExit):
         pass
 
+
+def extract_settings(options, settings):
+    import os
+    from flower.utils import abs_path, prepend_url
+
+    settings["debug"] = options.debug
+
+    if options.cookie_secret:
+        settings["cookie_secret"] = options.cookie_secret
+
+    if options.url_prefix:
+        for name in ["login_url", "static_url_prefix"]:
+            settings[name] = prepend_url(settings[name], options.url_prefix)
+
+    if options.auth:
+        settings["oauth"] = {
+            "key": options.oauth2_key or os.environ.get("FLOWER_OAUTH2_KEY"),
+            "secret": options.oauth2_secret or os.environ.get("FLOWER_OAUTH2_SECRET"),
+            "redirect_uri": options.oauth2_redirect_uri
+            or os.environ.get("FLOWER_OAUTH2_REDIRECT_URI"),
+        }
+
+    if options.certfile and options.keyfile:
+        settings["ssl_options"] = dict(
+            certfile=abs_path(options.certfile), keyfile=abs_path(options.keyfile)
+        )
+        if options.ca_certs:
+            settings["ssl_options"]["ca_certs"] = abs_path(options.ca_certs)
+
+
+def apply_env_options(options):
+    "apply options passed through environment variables"
+    import os
+
+    env_options = filter(is_flower_envvar, os.environ)
+    for env_var_name in env_options:
+        name = env_var_name.replace(ENV_VAR_PREFIX, "", 1).lower()
+        value = os.environ[env_var_name]
+        try:
+            option = options._options[name]
+        except KeyError:
+            option = options._options[name.replace("_", "-")]
+        if option.multiple:
+            value = [option.type(i) for i in value.split(",")]
+        else:
+            value = option.type(value)
+        setattr(options, name, value)
+
+
+def is_flower_envvar(name):
+    from flower.options import default_options
+
+    return (
+        name.startswith(ENV_VAR_PREFIX) and name[len(ENV_VAR_PREFIX) :].lower() in default_options
+    )
+
+
+def setup_logging(options):
+    from logging import NullHandler
+    from tornado.log import enable_pretty_logging
+
+    if options.debug and options.logging == "info":
+        options.logging = "debug"
+        enable_pretty_logging()
+    else:
+        logging.getLogger("tornado.access").addHandler(NullHandler())
+        logging.getLogger("tornado.access").propagate = False
+
+
+ENV_VAR_PREFIX = "FLOWER_"
 
 celery.add_command(flower)
