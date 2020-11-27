287d206078ca4da2b40c1e4852d1926b3ab4eb9e
Manuel Vázquez Acosta <manuel@merchise.org>
2020-11-27 00:43:26 -0500

    odoo jobs: Ensure task_time_limit is an integer.
    Fixes https://sentry.merchise.org/share/issue/ceb879a387b54b0daef0f3817f2aff5f/

--- odoo/jobs.py
+++ odoo/jobs.py
@@ -937,13 +937,17 @@ class Configuration(object):
 
     task_create_missing_queues = config.get("celery.create_missing_queues", True)
 
-    task_time_limit = config.get(
-        "celery.task_time_limit", os.environ.get("odoo_celery_task_time_limit", 600)  # 10 minutes
+    task_time_limit = int(
+        config.get(
+            "celery.task_time_limit",
+            os.environ.get("odoo_celery_task_time_limit", 600),  # 10 minutes
+        )
     )
     _softtime = config.get(
         "celery.task_soft_time_limit",
         os.environ.get(
-            "odoo_celery_task_soft_time_limit", 595  # 9min 55 seconds (5 seconds to finish)
+            "odoo_celery_task_soft_time_limit",
+            595,  # 9min 55 seconds (5 seconds to finish)
         ),
     )
     if _softtime is not None:
