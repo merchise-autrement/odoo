017b560c735920bf39ea30e0abbd8ccf9277c0c8
Manuel Vázquez Acosta <manuel@merchise.org>
2021-01-02 09:17:35 -0500

    crm, portal, *: Skip failing JS tests due to changes done by Merchise.

--- addons/portal/tests/__init__.py
+++ addons/portal/tests/__init__.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import test_load_process
+# merchise: Skip this test.  Our changes in addons 'xhg_ca_website_*' remove
+# part of the portal as Odoo ships it.  We should not run theses tests,
+# because the rely on the portal being unmodified.
+#
+# from . import test_load_process
--- addons/website_crm/tests/__init__.py
+++ addons/website_crm/tests/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import test_website_crm
+# merchise: Skip.  Our addon 'xhg_ca_website_theme' replaces most of the HTML
+# these rely on.  They simply can't work with those changes.
+#
+# from . import test_website_crm
