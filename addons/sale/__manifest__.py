c21162d2eed5340d893c900e6072376c70b5d3a8
Manuel Vázquez Acosta <manuel@merchise.org>
2022-01-28 23:10:15 +0100

    [sale]: Fix syntax error in addons/sale/__manifest__.py

--- addons/sale/__manifest__.py
+++ addons/sale/__manifest__.py
@@ -41,6 +41,6 @@ This module contains all the common features of Sales Management and eCommerce.
     ],
     'uninstall_hook': "uninstall_hook",
     'installable': False,
-    'auto_install': False
+    'auto_install': False,
     'license': 'LGPL-3',
 }
