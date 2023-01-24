c220ad2ef1df73ab4597d34dbd5e359853e02e25
Manuel Vázquez Acosta <manuel@merchise.org>
2023-01-24 18:36:49 +0100

    merchise: Remove optional ldap dep.

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
         'psutil',  # windows binary code.google.com/p/psutil/downloads/list
         'psycopg2 >= 2.2',
         'pydot',
-        'pyldap',  # optional
         'pyparsing',
         'pypdf2',
         'pyserial',
